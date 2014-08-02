9700e31b3370f752da0597cb64565eb8cc4669f0
Stephanie Nguyen <stephanie@silicalabs.com>
2014-08-02 01:20:27 -0400

    movies lab

--- playtime/lesson02_movies.py
+++ playtime/lesson02_movies.py
@@ -1,5 +1,7 @@
 # Goal #1: Create a program that will print out a list of movie titles and a set of ratings defined below into a particular format.
 
+
+
 # First, choose any five movies you want.
 
 # Next, look each movie up manually to find out four pieces of information:
@@ -14,6 +16,12 @@
 
 # You'll need a variable for movie_titles, a variable for parental_rating, a variable for bechdel_rating, a variable for imdb_rating, and a variable for genre.
 
+movie_titles = ['101 Dalmations','13 Going on 30','American Zombie','Food Stamped','The Bourne Identity']
+parental_rating = ['G','PG-13','Unrated','R','G']
+bechdel_rating = ['3/3', '3/3', '3/3', '3/3', '1/3']
+imdb_rating = ['7.2','6.1','5.1', '6.5', '7.9']
+genre = ['Animation','Comedy','Horror','Documentary','Thriller']
+
 # Since you have five sets of facts about five movies, you'll want to use lists to hold these pieces of information.
 
 # Once all of your information is stored in lists, loop through those lists to print out information with each part separated by a comma, like this:
@@ -22,9 +30,22 @@
 # Jurassic Park, PG-13, 3, 8.0, Adventure / Sci-Fi
 # Back to the Future, PG, 1, 8.5, Adventure / Comedy / Sci-Fi
 
+dataArray = zip(movie_titles, parental_rating, bechdel_rating, imdb_rating, genre)
+for movie_titles, parental_rating, bechdel_rating, imdb_rating, genre in dataArray:
+    print "{0}, {1}, {2}, {3}, {4}".format(movie_titles, parental_rating, bechdel_rating, imdb_rating, genre)
+
 # Note how each piece of information is separated by a comma.  This is a specific file format called the "Comma Separated Value (CSV)" format
 # If you can make a CSV file, you can open it up in Excel or Numbers as a spreadsheet.
 
 # When you've printed out your information like the example above, copy/paste that into a file and save it as a .csv file.
 # Open that up in Excel, Numbers, or another spreadsheet program.  How does it look?
 # To see an example of how it should look, check out: https://github.com/shannonturner/python-lessons/blob/master/section_05_(loops)/movies.csv
+
+
+
+
+
+
+
+
+
